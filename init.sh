3e3ea03715e306ad3acb50ee162361fce9a04440
ckrichardson <clifford_richardson@nevada.unr.edu>
2023-02-18 17:19:53 -0800

    add cron to init script

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
+pwd=$(pwd)
+
 if [ $(crontab -l | wc -c) -eq 0 ]; 
 then
-  echo "crontab is empty"
+  echo "0 9 * * fri python3 $pwd/src/chore-picker.py" | crontab -
 else
-  echo "crontab is not empty"
+  (crontab -l ; echo "0 9 * * fri python3 $pwd/src/chore-picker.py") | crontab -
 fi
